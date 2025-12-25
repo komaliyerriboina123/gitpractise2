3fd2c9abcf0115d11a3ad52c80288dfcafb78701
komaliyerriboina123 <yerriboinakomali@gmail.com>
2025-12-25 07:56:41 +0530

    Add script to sum two user-input numbers

---
+++ add.py
@@ -0,0 +1,11 @@
+# add_two_numbers.py
+
+# Taking input from user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Adding two numbers
+sum = num1 + num2
+
+# Printing result
+print("Sum of two numbers is:", sum)
